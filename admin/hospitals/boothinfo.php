db093e203e21584fdfbf4546a10c6889a2d50e77
shakawaat067 <mhossain162067@bscse.uiu.ac.bd>
2022-09-18 21:41:06 +0600

    update registrationinfo

---
+++ admin/hospitals/boothinfo.php
@@ -0,0 +1,132 @@
+<?php
+require '../../config/adminsession.php';
+require '../../config/conn.php';
+include '../../includes/header.php';
+
+?>
+
+<header class="d-flex justify-content-center py-3 bg-secondary">
+        <h1 style="color:white">Booth Info</h1>
+        </header>
+<table>
+<tr>
+ 
+    <th>Hospital_Name </th>
+    <th>Localtion</th>
+      <th>Floor</th>
+ 
+    <th>Wing</th>
+    <th>Section</th>
+    
+   
+    
+</tr>
+
+<?php
+
+
+
+$selectData = 'SELECT booth.* , hospital.Hospital_Name , cities.city_name FROM `booth` INNER JOIN hospital on booth.hospital_id = hospital.id  INNER JOIN cities on booth.city_id = cities.id';
+$result = mysqli_query($conn, $selectData);
+while($row = mysqli_fetch_assoc($result)){
+     $id =  $row['id'];
+  
+     $Hospital_Name  =  $row['Hospital_Name'];
+     $Location =  $row['city_name'];
+     $Floor   =  $row['Floor'];
+     $Wing  =  $row['Wing'];
+     $Section =  $row['Section'];
+ 
+   ?>
+
+
+
+
+<tr>
+    <td><?php echo $Hospital_Name; ?></td>
+    <td><?php echo $Location; ?></td>
+ 
+    <td><?php echo $Floor; ?></td>
+    <td><?php echo $Wing; ?></td>
+    <td><?php echo $Section; ?></td>
+  
+ 
+</tr>
+
+
+ 
+
+<?php
+}
+
+ 
+
+?></table>
+
+<?php include '../../includes/footer.php'; ?>
+<?php
+require '../../config/adminsession.php';
+require '../../config/conn.php';
+include '../../includes/header.php';
+
+?>
+
+<header class="d-flex justify-content-center py-3 bg-secondary">
+        <h1 style="color:white">Booth Info</h1>
+        </header>
+<table>
+<tr>
+ 
+    <th>Hospital_Name </th>
+    <th>Localtion</th>
+      <th>Floor</th>
+ 
+    <th>Wing</th>
+    <th>Section</th>
+    
+   
+    
+</tr>
+
+<?php
+
+
+
+$selectData = 'SELECT booth.* , hospital.Hospital_Name , cities.city_name FROM `booth` INNER JOIN hospital on booth.hospital_id = hospital.id  INNER JOIN cities on booth.city_id = cities.id';
+$result = mysqli_query($conn, $selectData);
+while($row = mysqli_fetch_assoc($result)){
+     $id =  $row['id'];
+  
+     $Hospital_Name  =  $row['Hospital_Name'];
+     $Location =  $row['city_name'];
+     $Floor   =  $row['Floor'];
+     $Wing  =  $row['Wing'];
+     $Section =  $row['Section'];
+ 
+   ?>
+
+
+
+
+<tr>
+    <td><?php echo $Hospital_Name; ?></td>
+    <td><?php echo $Location; ?></td>
+ 
+    <td><?php echo $Floor; ?></td>
+    <td><?php echo $Wing; ?></td>
+    <td><?php echo $Section; ?></td>
+  
+ 
+</tr>
+
+
+ 
+
+<?php
+}
+
+ 
+
+?></table>
+
+<?php include '../../includes/footer.php'; ?>
